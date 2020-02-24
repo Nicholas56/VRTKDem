3c3a239d9ce6a16dd4b3423002ea20b646fabe87
Nicholas56 <nick.easterby@gmail.com>
2020-02-24 14:43:26 +0000

    Added pancake Scene

---
+++ Assets/Scripts/MakePancakes.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MakePancakes : VRTK_InteractableObject
+{
+    public Transform spawnPoint;
+    public int numOfPancakes;
+    float spawnDelay = 0.5f;
+    float timer;
+
+    bool makePancakes;
+    public GameObject pancake;
+
+    protected VRTK_ControllerEvents controllerEvents;
+
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        controllerEvents = currentUsingObject.GetComponent<VRTK_ControllerEvents>();
+    }
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        controllerEvents = null;
+    }
+
+    
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (controllerEvents)
+        {
+            float power = controllerEvents.GetTriggerAxis();
+            if (Time.time > timer)
+            {
+                TurnOnMachine();
+                timer = Time.time + spawnDelay;
+            }
+            VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), power * 0.25f, 0.1f, 0.01f);
+        }
+        else
+        {
+        }
+    }
+
+    void TurnOnMachine()
+    {
+        Instantiate(pancake, spawnPoint.position, Quaternion.identity);
+    }
+}
