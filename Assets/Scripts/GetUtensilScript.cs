bef4e3f3b8d9c7bc443364d7ab528f011224b258
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2020-02-24 18:18:11 +0000

    Added burnt Pancakes

---
+++ Assets/Scripts/GetUtensilScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GetUtensilScript : VRTK_InteractableObject
+{
+    public Transform spawnPoint;
+    float spawnDelay = 0.5f;
+    float timer;
+
+    bool makePancakes;
+    public GameObject utensil;
+    GameObject madeItem;
+
+    protected VRTK_ControllerEvents controllerEvents;
+
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        controllerEvents = currentUsingObject.GetComponent<VRTK_ControllerEvents>();
+    }
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        controllerEvents = null;
+    }
+
+    private void Start()
+    {
+        TurnOnMachine();
+        Debug.Log("Started");
+    }
+
+
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (controllerEvents)
+        {
+            float power = controllerEvents.GetTriggerAxis();
+            if (Time.time > timer)
+            {
+                Destroy(madeItem);
+                TurnOnMachine();
+                timer = Time.time + spawnDelay;
+            }
+            VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), power * 0.25f, 0.1f, 0.01f);
+        }
+        else
+        {
+        }
+    }
+
+    void TurnOnMachine()
+    {
+        madeItem = Instantiate(utensil, spawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PancakeScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PancakeScript : MonoBehaviour
+{
+    bool isSticky = false;
+    float stickVelocity = 10f;
+
+    public Image topSide;
+    public Image bottomSide;
+
+    float topCooked = 0;
+    float bottomCooked = 0;
+
+    float valTop =-1f;
+    float valBottom=-1f;
+
+    bool upRight;
+    float threshold = 0.6f;
+
+    public Color uncooked;
+    public Color cooked;
+    public Color burnt;
+
+    Rigidbody rb;
+    GameObject hob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        hob = GameObject.FindGameObjectWithTag("Hob");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > stickVelocity) { isSticky = true; }
+        if (IsUpright()) { upRight = true; } else { upRight = false; }
+
+        if (topCooked != valTop || bottomCooked != valBottom)
+        {
+            if (topCooked < 20) { topSide.color = uncooked; }
+            else if (topCooked > 20 && topCooked < 60) { topSide.color = cooked; }
+            else if (topCooked > 60) { topSide.color = burnt; }
+            valTop = topCooked;
+
+            if (bottomCooked < 20) { bottomSide.color = uncooked; }
+            else if (bottomCooked > 20 && bottomCooked < 60) { bottomSide.color = cooked; }
+            else if (bottomCooked > 60) { bottomSide.color = burnt; }
+            valBottom = bottomCooked;
+        }
+
+        if (Vector3.Distance(transform.position, hob.transform.position) < 0.5f)
+        {
+            CookSide();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isSticky)
+        {
+            transform.SetParent(collision.transform);
+        }
+    }
+    public bool IsUpright()
+    {
+        return transform.up.y > threshold;
+    }
+
+    public void CookSide()
+    {
+        if (upRight)
+        {
+            topCooked += Time.deltaTime;
+        }
+        else
+        {
+            bottomCooked += Time.deltaTime;
+        }
+    }
+}
